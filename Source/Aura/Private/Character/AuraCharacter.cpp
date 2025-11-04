1e2f5f10199f80ba5e64b69c980fbd1a57021db6
Rudyip <2366227308@qq.com>
2025-11-05 00:28:50 +0800

    初始化Aura角色相关移动设置

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,20 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	// 角色朝向移动的方向旋转
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	// 设置水平旋转的速度
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+	// 把角色移动约束在平面上
+	GetCharacterMovement()->bConstrainToPlane = true;
+	// 开始就把角色放到平面上
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+	/* 关闭控制器旋转控制 */
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+}
