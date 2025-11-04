83b1978d1ef5809accfcae28b14b730a8c827f0a
Rudyip <2366227308@qq.com>
2025-11-04 23:33:07 +0800

    设置移动输入组件，添加移动输入，新增玩家控制器蓝图

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,6 +3,7 @@
 
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
@@ -31,4 +32,32 @@ void AAuraPlayerController::BeginPlay()
 	// 鼠标不锁定在视口内
 	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	// 获取增强输入组件
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	// 绑定移动输入处理函数
+	EnhancedInputComponent->BindAction(InputAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	if (APawn* ControllerPawn = GetPawn<APawn>())
+	{
+		const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+		const FRotator Rotator = GetControlRotation();
+		// Yaw轴偏转角度（水平偏转角度）
+		const FRotator YawRotator(0.f, Rotator.Yaw, 0.0f);
+		// 前后方向
+		const FVector ForwardDirection = FRotationMatrix(YawRotator).GetUnitAxis(EAxis::X);
+		// 左右方向
+		const FVector RightDirection = FRotationMatrix(YawRotator).GetUnitAxis(EAxis::Y);
+		// 添加前后移动输入
+		ControllerPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		// 添加左右移动输入
+		ControllerPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
 }
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -6,10 +6,12 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+struct FInputActionValue;
+class UInputAction;
 class UInputMappingContext;
 
 /**
- * 
+ * Aura玩家控制器
  */
 UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
@@ -19,8 +21,20 @@ public:
 	AAuraPlayerController();
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 private:
 	// 输入映射上下文
 	UPROPERTY(EditAnywhere, Category="Input")
 	TObjectPtr<UInputMappingContext> AuraContext;
+
+	// 输入操作
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> InputAction;
+
+	/**
+	 * 移动
+	 * 
+	 * @param InputActionValue 输入值 
+	 */
+	void Move(const FInputActionValue& InputActionValue);
 };
